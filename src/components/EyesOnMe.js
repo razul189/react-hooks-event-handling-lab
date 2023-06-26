0f54b46ba8d103b69f45111e165e4d30cdf0185e
Ramia Zulfiqar <razulfiqar1@gmail.com>
2023-06-26 16:43:46 -0400

    lab completed

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,18 @@
 // Code EyesOnMe Component Here
+
+function EyesOnMe () {
+    function handleBlur () {
+        console.log ('Hey! Eyes on me!')
+    }
+    function handleFocus () {
+        console.log ("Good!")
+    }
+    return (
+            <button onBlur = {handleBlur} onFocus = {handleFocus}> 
+             Eyes on me
+            </button>
+
+    )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,16 @@
 // Code Keypad Component Here
 
 function Keypad (){
+    function typing () {
+        console.log("Entering password...")
+    }
     return (
-        <div></div>
+        <div> 
+            <input 
+            type = "password"
+            onChange ={typing}
+            />
+        </div>    
     )
 }
 
